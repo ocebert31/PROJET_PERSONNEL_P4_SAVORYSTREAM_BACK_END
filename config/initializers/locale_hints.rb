91b1da741b1b77a3e0d4a1134090da2fdf55f685
ocebert31 <oceanebertrand339@gmail.com>
2026-06-02 13:49:04 +0200

    fix bug

--- config/initializers/locale_hints.rb
+++ config/initializers/locale_hints.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-# Ce fichier s'exécute au démarrage de Rails et définit 
+
+# Ce fichier s'exécute au démarrage de Rails et définit
 # le marché par défaut du serveur : celui utilisé lorsque le client n'envoie pas de région exploitable dans Accept-Language.
 
 alpha2 = ENV.fetch("DEFAULT_MARKET_COUNTRY_ALPHA2", "FR").to_s.strip.upcase
